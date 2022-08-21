f90a6b8b27f33d70214a47ec786e35102cc6309b
FMotalleb <fmotalleb@gmail.com>
2022-08-21 08:16:02 +0430

    [fix] gitignor

--- k_means_rust/target/debug/deps/threadpool-c223a0931b90b7fa.d
+++
@@ -1,5 +0,0 @@
-c:\Users\motalleb\Documents\GitHub\rust_test\k_means_rust\target\debug\deps\threadpool-c223a0931b90b7fa.rmeta: C:\Users\motalleb\.cargo\registry\src\github.com-1ecc6299db9ec823\threadpool-1.8.1\src\lib.rs
-
-c:\Users\motalleb\Documents\GitHub\rust_test\k_means_rust\target\debug\deps\threadpool-c223a0931b90b7fa.d: C:\Users\motalleb\.cargo\registry\src\github.com-1ecc6299db9ec823\threadpool-1.8.1\src\lib.rs
-
-C:\Users\motalleb\.cargo\registry\src\github.com-1ecc6299db9ec823\threadpool-1.8.1\src\lib.rs:
